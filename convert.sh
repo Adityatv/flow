ea1af2b8c01c92e9b69da4361cf63a177a9fdd4a
pk3011 <118091725+pk3011@users.noreply.github.com>
2024-02-15 13:04:50 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,8 +1,8 @@
 # Change input and output details
 
-output_path="big-buck-bunny" # Output Path (Same will be pushed in main also)
-input_url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" # Input direct file url
-input_extension="mp4" # Extension of file url
+output_path="Dubki" # Output Path (Same will be pushed in main also)
+input_url="https://yhills.chammii.workers.dev/0:/Dunki.2023.HINDI.720p.10bit.WEBRip.2CH.x265.HEVC-PSA.mkv" # Input direct file url
+input_extension="mkv" # Extension of file url
 
 
 
